575efc23c05ddcc8b0dd2a25a706852fb0a4fee2
MauriceWagner224 <79831881+MauriceWagner224@users.noreply.github.com>
2023-07-11 10:22:55 +0200

    Refactor, Pausefunction, Maploading and restarting
    Game Class is now called Gameloop
    Pausing has to option to restart or Exit the Game
    Coins are stored from previous Levels when restarting a Game and being further than world1
    Instructions on how to pause are drawn
    restarting world-fixes

--- platformer.py
+++ platformer.py
@@ -1,15 +1,19 @@
 import pygame
-from maps import map1, map2
+
+from worlds import world1, world2
+from worldloader import WorldLoader
 from properties import *
 from interface import UserInterface
 from player import Player
-from world import World
+
+
 
 
 ## Starting and updating of every main Class, that's needed for the Game
-class Game:
+class GameLoop:
 	""" init basic pygame parameter and init variables for Gameloop-Conditioning when Pausing etc.
 	 	matchworld stores the number when loading a world | currentworld stores the actual map_data and is determined by matchworld"""
+
 	def __init__(self):
 		# basic pygame parameters,
 		pygame.init()
@@ -17,10 +21,10 @@ class Game:
 		self.screen = pygame.display.set_mode((screen_width, screen_height))
 		pygame.display.set_caption(windows_caption)
 		# init. gameloop-vars to condition breakpoints when pausing, dying etc.
-		self.paused = False
-		self.main_menu = True
+
 		self.matchworld = 1
-		self.currentworld = None
+		self.previousCoins = 0
+
 
 	# actual gameloop
 	def run(self):
@@ -29,38 +33,54 @@ class Game:
 			self.screen.blit(bg_img, (0, 0))
 			self.screen.blit(sun_img, (100, 100))
 
-			""" If conditions stops updating of the game-elements, when paused or dead, only background image above is drawn"""
+
+			""" If conditions stops updating of the game-elements, when paused or dead, only background image above is drawn+keyinput"""
 			if self.main_menu:
 				if self.userInterface.exit_button.draw():
 					self.run = False
 				if self.userInterface.start_button.draw():
 					self.main_menu = False
 			elif self.paused:
-				if self.userInterface.resume_button.draw():
-					self.paused = not self.paused
+				if self.userInterface.restart_button.draw():
+					self.reset()
+				if self.userInterface.exit_button.draw():
+					self.run = False
 			elif self.player.isdead:
 				if self.userInterface.restart_button.draw():
 					self.reset()
-					self.start(self.currentworld)
+
+					#self.start(self.currentworld)
 			else:
 				self.update()
-				pygame.display.flip()
 
+			"""
+			Another conditioned method, that should always be active, no matter the gamestate
+			Instructions to pause/resume the game, that are using the same function as the coins, shown in the top left corner. Because of Text size and position on screen.
+			the x and y pos are fractions of screenwidth/screenheight 
+			"""
+			if self.paused:
+				self.userInterface.drawText('Press "P" to resume the Game', self.screen,self.screen.get_width() / 9.85,self.screen.get_height() / 1.075, 16)
+			else:
+				self.userInterface.drawText('Press "P" to pause the Game', self.screen,self.screen.get_width() / 7.5,self.screen.get_height() / 1.075, 16)
+			pygame.display.flip()
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					self.run = False
-				if event.type == pygame.KEYUP:
+				if event.type == pygame.KEYUP and self.main_menu ==  False:
 					if event.key == pygame.K_p:
 						self.paused = not self.paused
 
 			pygame.display.update()
 		# when run == False, pygame.quit event
-		self.quit()
+		pygame.quit()
 
 
 	""" initiating class instances, sprite-groups, a list containing the world_data tiles
-		then run gameloop"""
+		then run gameloop
+	"""
 	def start(self, map_data, coins = None):
+		self.paused = False
+		self.main_menu = True
 		self.userInterface = UserInterface(self.screen)
 		self.userInterface.initButtons()
 		self.currentworld = map_data
@@ -82,18 +102,21 @@ class Game:
 		self.tile_list = []
 
 		# reference this instance of Game() to grand world and player access to Gameclass-vars
-		self.world = World(self, map_data)
+		self.world = WorldLoader(self, map_data)
 		self.player = Player(100, screen_height - 250, self)
-
 		self.run()
 
 
-	""" nextWorld counts up the variable(matchworld) after completing the currentlevel. the number of matchworld is matched to a corresponding worldfile """
+	""" nextWorld counts up the variable(matchworld) after completing the currentlevel. the number of matchworld is matched to a corresponding worldfile 
+		coins are saved in the variable previousCoins, so they ONLY reset(to the amount of the last level) if a level restartet through death/menu
+	"""
 	def nextWorld(self):
 		self.matchworld += 1
 		self.reset(self.coins)
 
-	""" resets the player and world to load the next world, instances are deleted and spritegroups cleared"""
+	""" resets the player and world to load the next world, instances are deleted and spritegroups cleared
+		paused/main
+	"""
 	def reset(self, coins = None):
 		self.platform_group.empty()
 		self.missile_sprites.empty()
@@ -102,24 +125,33 @@ class Game:
 		self.player_sprites.empty()
 		self.ExitsGroup.empty()
 		self.CoinGroup.empty()
+		del self.userInterface.exit_button
+		del self.userInterface.restart_button
+		del self.userInterface.start_button
+		del self.enemy_sprites
+		del self.currentworld
 		del self.tile_list
 		del self.world
 		del self.player
-		# matchworld stores a number that is matched to the worlds
+		self.coins = 0
+
+		# matchworld stores a number, that is matched in a switch/case-statement to load the  mapdata of currentworld or the next world to load
 		match self.matchworld:
 			case 1:
-				self.start(map1.map_data, coins)
+				self.start(world1.world_data, self.previousCoins)
 			case 2:
-				self.start(map2.map_data, coins)
+				self.start(world2.world_data, self.previousCoins)
 			case 3:
-				self.start(map3)
+				self.start(world3)
 
-	"""most game-intern updating should happen here"""
+
+	"""most game-internal updating should happen here"""
 	def update(self):
 		self.player.update()
 		self.all_sprites.update()
 		self.all_sprites.draw(self.screen)
-		self.userInterface.drawText(self.coins, self.screen, 20, 20)
+		self.userInterface.drawText(self.coins, self.screen, 25, 20, 32)
+
 
 	def quit(self):
 		pygame.quit()
